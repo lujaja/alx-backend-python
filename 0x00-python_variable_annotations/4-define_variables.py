bf82dc75eb28b42d203fad6be89a9cf0823cd5c2
lujaja <jarzcyber@gmail.com>
2024-05-03 17:08:01 +0300

    variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Define variables with specific types
-    a: an integer with a value of 1
-    pi: a float with a value of 3.14
-    i_understand_annotations: a boolean with a value of True
-    school: a string with a value of "Holberton"
+    a, an integer with a value of 1
+    pi, a float with a value of 3.14
+    i_understand_annotations, a boolean with a value of True
+    school, a string with a value of "Holberton"
 """
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "Holberton"
+school: str = 'Holberton'
