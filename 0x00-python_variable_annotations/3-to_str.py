2910cc6fa33a818095d77c2ef5cf999e7ccfbd57
lujaja <jarzcyber@gmail.com>
2024-05-03 16:45:04 +0300

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function - to_str
+
+    Attributes:
+        n (float): parameter
+
+    Return:
+        n (stry): convert to string
+    """
+    return (str(n))
