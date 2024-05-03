cef4c7827367522b2303e79c5f89178d1650486b
lujaja <jarzcyber@gmail.com>
2024-05-03 16:28:25 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotation - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concat
+
+    Attribute:
+        str1 (str): parameter
+        str2 (str): parameter
+    """
+    return (str1 + str2)
