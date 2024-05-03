3f484835f22e2764259a1b25905c0f7ad6599c16
lujaja <jarzcyber@gmail.com>
2024-05-03 16:21:48 +0300

    anotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Fucntion add
+
+    Attribute:
+        a (float): paratmeter 1
+        b (float): parameter 2
+    """
+    return (a + b)
