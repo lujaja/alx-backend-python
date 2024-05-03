50163cd5c5d09cd69c69ca2ca6d77739790902b3
lujaja <jarzcyber@gmail.com>
2024-05-03 17:04:42 +0300

    variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Define variables
+Define variables with specific types
+    a: an integer with a value of 1
+    pi: a float with a value of 3.14
+    i_understand_annotations: a boolean with a value of True
+    school: a string with a value of "Holberton"
 """
 
 
-# integer value
 a: int = 1
-# A float value
 pi: float = 3.14
-# A boolean with a value of True
 i_understand_annotations: bool = True
-# A string
 school: str = "Holberton"
