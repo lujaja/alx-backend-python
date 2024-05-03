84d3c9e903918c2b1931cbd625d682a0c814a77f
lujaja <jarzcyber@gmail.com>
2024-05-03 17:27:02 +0300

    list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Complex Types - list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Fucntion - sum_list
+
+    Attributes:
+        input_list (list): paramater
+
+    Return:
+        sum (float): The sum of the input_list
+    """
+    return (sum(input_list))
