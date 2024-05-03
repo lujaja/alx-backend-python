0f83b61f710f22e775a4bdff3721d77e0a561381
lujaja <jarzcyber@gmail.com>
2024-05-03 19:36:25 +0300

    advanced

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,21 @@
-# The types of the elements of the input are not know
-def safe_first_element(lst):
+#!/usr/bin/env python3
+"""
+100-safe_first_element.py
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    Returns the first element of the input list if the list is not empty, otherwise returns None.
-    
-    :param lst: A list of elements.
-    :type lst: list
-    :return: The first element of the input list if the list is not empty, otherwise None.
-    :rtype: Any or None
+    Returns the first element of the given list if it is not
+     empty, otherwise returns None.
+
+    :param lst: A sequence of elements.
+    :type lst: Sequence[Any]
+    :return: The first element of the list if it is not empty, otherwise None.
+    :rtype: Union[Any, None]
     """
-    return lst[0] if lst else None
+    if lst:
+        return lst[0]
+    else:
+        return None
