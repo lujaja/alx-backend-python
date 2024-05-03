02840110cad7dbddb0495b587dc6ece551b743ec
lujaja <jarzcyber@gmail.com>
2024-05-03 16:39:10 +0300

    2-floor

--- 0x00-python_variable_annotations/1-main.py
+++
@@ -1,7 +0,0 @@
-concat = __import__('1-concat').concat
-
-str1 = "egg"
-str2 = "shell"
-
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic anotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function - floor
+
+    Attributes:
+        n (float) parameter
+
+    Return:
+        n (floor)
+    """
+    return (math.floor(n))
