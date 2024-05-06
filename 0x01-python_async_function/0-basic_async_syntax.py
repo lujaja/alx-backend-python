451399df3af6ef265a3aded849de7383bf911612
lujaja <jarzcyber@gmail.com>
2024-05-06 18:46:14 +0300

    basic asynchronous function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,3 +2,19 @@
 """
 The basic of asynchronous function
 """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10.0) -> float:
+    """
+    Function - wait_random
+
+    Attributes:
+        max_delay (float): default value 10.0
+
+    Return (float): the delay waited
+    """
+    delay: float = random.uniform(0.0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
