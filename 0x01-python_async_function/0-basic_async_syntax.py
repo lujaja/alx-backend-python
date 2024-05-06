74f56bf0395c866bd641011cbae4d1fc62975839
lujaja <jarzcyber@gmail.com>
2024-05-06 19:09:36 +0300

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,11 +9,11 @@ Use the random module.
 """
 
 
-import asyncio
 import random
+import asyncio
 
 
-async def wait_random(max_delay: float = 10.0) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Function - wait_random
 
