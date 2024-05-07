54789e75bb103017f4a55b4b888a896b18dd3ad2
lujaja <jarzcyber@gmail.com>
2024-05-07 18:47:32 +0300

    update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,4 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Function - task_wait_n
+    """
     return await wait_n(n, max_delay)
